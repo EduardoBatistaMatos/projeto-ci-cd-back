dbceec27e11af79fa8d10c4ba9d57335538e0227
Eduardo <dudubmatos@gmail.com>
2025-11-14 13:13:38 -0300

    chore: atualiza URL de conexão com frontend

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const port = process.env.PORT || 3000;
 
 const app = express();
 app.use(cors({
-  origin: "https://projeto-ci-cd-front-omega.vercel.app/"
+  origin: "https://projeto-ci-cd-front-omega.vercel.app"
 }));
 
 
